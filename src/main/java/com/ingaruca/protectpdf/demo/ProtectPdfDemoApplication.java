1f038381dc19f79d741a7686fa2205289514d8b8
Luis Ingaruca <luise.ingaruca@gmail.com>
2019-07-21 13:47:24 -0500

    Agregando dependencia lombok en archvio POM

--- src/main/java/com/ingaruca/protectpdf/demo/ProtectPdfDemoApplication.java
+++ src/main/java/com/ingaruca/protectpdf/demo/ProtectPdfDemoApplication.java
@@ -1,6 +1,7 @@
 package com.ingaruca.protectpdf.demo;
 
 import com.ingaruca.protectpdf.demo.service.PDFService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -10,6 +11,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Base64;
 
+@Slf4j
 @SpringBootApplication
 public class ProtectPdfDemoApplication implements CommandLineRunner {
 
@@ -22,15 +24,15 @@ public class ProtectPdfDemoApplication implements CommandLineRunner {
 
   @Override
   public void run(String... args) throws Exception {
-    System.out.println("Creating PDF.");
+    log.info("Creating PDF.");
     pdfService.createPdf();
-    System.out.println("PDF creation done.");
+    log.info("PDF creation done.");
 
     byte[] input = Files.readAllBytes(Paths.get("/Users/ingarukadev/Documents/Projects/protect-pdf/message.pdf"));
     byte[] encodedInput = Base64.getEncoder().encode(input);
 
     String fileInBase64 = new String(encodedInput);
 
-    System.out.println("base 64: " + fileInBase64);
+    log.info("base 64: " + fileInBase64);
   }
 }
